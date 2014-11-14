73774fb0716a61796b5f7c76311963bb9e451ea3
Sam Plews <the@codeite.com>
2014-11-14 14:21:41 +0000

    Added Cost by Day

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var moment = require('moment');
 
-var mongodbUrl = "mongodb://courtsdocs:615iZrDh6Fwi50q@ds051740.mongolab.com:51740/courts_docs_stats"
-//var mongodbUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/CourtDocuments";
+//var mongodbUrl = "mongodb://courtsdocs:615iZrDh6Fwi50q@ds051740.mongolab.com:51740/courts_docs_stats"
+var mongodbUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/CourtDocuments";
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -24,6 +24,10 @@ router.get('/', function(req, res) {
 /* GET home page. */
 router.get('/cost', function(req, res) {
   res.render('cost');
+});
+
+router.get('/cost-by-day', function(req, res) {
+  res.render('cost-by-day');
 });
 
 router.get('/data', function(req, res) {
@@ -166,6 +170,68 @@ router.get('/data-cost', function(req, res) {
         var count = (dataMap[now.format("YYYY-MM-DDTHH")] || 0)/100;
         cost += count;
         dataSeries.push("  ["+newDateString+", "+cost+"]")
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end("[\n" + dataSeries.join(",\n") + "\n]");
+
+    });
+  });
+});
+
+
+router.get('/data/cost-by-day', function(req, res) {
+
+  MongoClient.connect(mongodbUrl, function(err, db) {
+    if(err) throw err
+
+    var collection = db.collection('updates');
+
+
+    var aggQuery = [
+      {"$unwind": "$documents"},
+      // {"$match": {"documents.numberOfPages": { $gt: 20} } },
+      { "$project": {
+        publishedDate: "$publishedDate",
+        pages: "$documents.numberOfPages",
+        cost:  {$multiply: ["$documents.numberOfPages", 10]}
+      }},
+
+
+      { "$group": {
+          _id: { $substr: [ "$publishedDate", 0, 10 ]},
+          docs: { $sum: 1 },
+          pages: { $sum: "$pages" },
+          cost: { $sum: "$cost" }
+      }}
+
+    ];
+
+    collection.aggregate(aggQuery, function(err, result) {
+
+      var dataMap = {};
+      var earliest = '9999-01-01T00:00:00Z';
+      var latest = '0000-01-01T00:00:00Z';
+
+      result.forEach(function(it){
+        dataMap[it._id] = it.cost;
+
+        if(it._id < earliest) earliest = it._id;
+        if(it._id > latest) latest = it._id;
+      });
+
+      earliest = moment(earliest).add(-1, 'day');
+      latest = moment(latest);
+
+      var dataSeries = []
+
+      var cost = 0;
+      for(var now = earliest.clone(); now <= latest; now.add(1, 'day')) {
+
+        var newDateString = 'new Date('+now.year()+', '+(now.month())+', '+now.date()+', '+now.hour()+')';
+        var count = (dataMap[now.format("YYYY-MM-DD")] || 0)/100;
+
+        dataSeries.push("  ["+newDateString+", "+count+"]")
       }
 
       res.setHeader('Content-Type', 'application/json');
